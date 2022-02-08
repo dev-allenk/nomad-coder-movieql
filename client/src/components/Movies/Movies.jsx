fffeea43c8595099f42235fca995ea07557511e1
dev.allenk <dev.allenk@gmail.com>
2022-02-08 23:03:13 +0900

    feat: Added link between Home and Detail

--- client/src/components/Movies/Movies.jsx
+++ client/src/components/Movies/Movies.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import * as S from "./style"
+import { Link } from "react-router-dom"
 
 export default function Movies({ movies }) {
   return (
     <S.Movies>
       {movies.map((movie) => (
         <S.Movie key={movie.id}>
-          <img src={movie.medium_cover_image} />
+          <Link to={`/${movie.id}`}>
+            <img src={movie.medium_cover_image} />
+          </Link>
         </S.Movie>
       ))}
     </S.Movies>
